cbfc555213e9eb940d9e6612012caf3b222d26cf
Anushka172000 <94109029+Anushka172000@users.noreply.github.com>
2023-02-02 19:53:18 +0530

    Add files via upload

---
+++ Practical/Constructor.java
@@ -0,0 +1,16 @@
+class My{
+    int a;
+    String name;
+    public void display(){
+         System.out.println(a+""+ name);
+    }
+My(){
+    System.out.println("hiiiiii");
+
+    }
+    public static void main(String args[])
+    {
+       My b=new My();
+       b.display();
+    }
+}
