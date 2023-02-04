1ae3523ef63ead8395ca73617f5971f88d81ab7f
Anushka172000 <94109029+Anushka172000@users.noreply.github.com>
2023-02-04 12:15:44 +0530

    Add files via upload

---
+++ Lab/divisible.java
@@ -0,0 +1,19 @@
+import java.util.*;
+ 
+public class divisible{
+    	public static void main(String args[]) {
+					
+				
+		System.out.println("\n\nDivided by 5: ");
+		for (int i=1; i<=100; i++) { //initializing, giving the range and doing postinrement also 
+			if (i%5==0 && i>40) { //it checks whether the numb is divided by 5 and whether it is greater than 40 or not
+				
+				System.out.print(i +" ");
+			}
+				
+		}
+		
+				
+		System.out.println("\n");
+  }
+}
