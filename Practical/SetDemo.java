d451cf2a421fbc39b6dc7a187fddbad16368b509
Anushka172000 <94109029+Anushka172000@users.noreply.github.com>
2023-03-27 21:35:06 +0530

    Add files via upload

---
+++ Practical/SetDemo.java
@@ -0,0 +1,19 @@
+import java.util.Set;
+import java.util.HashSet;
+
+
+public class SetDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Set<Integer> set = new HashSet<Integer>();
+set.add(10);
+set.add(12);
+set.add(13);
+set.add(18);
+set.add(20);
+
+System.out.println(set);
+	}
+
+}
